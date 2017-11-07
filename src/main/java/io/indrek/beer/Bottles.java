3651218d9faa84e1e76098e52fc6a82d7410afae
Indrek Ots <ots.indrek@gmail.com>
2017-11-07 22:20:49 +0100

    Create basic verse constructor
    Add a very basic verse constructor which passes all tests.

--- src/main/java/io/indrek/beer/Bottles.java
+++ src/main/java/io/indrek/beer/Bottles.java
@@ -1,7 +1,12 @@
 package io.indrek.beer;
 
 public class Bottles {
+
+    private final String VERSE_TEMPLATE = "%d bottles of beer on the wall, %d bottles of beer.\n" +
+            "Take one down and pass it around, %d bottles of beer on the wall.";
+
     public String verse(int verseNumber) {
-        return null;
+        return String.format(VERSE_TEMPLATE, verseNumber, verseNumber, verseNumber - 1);
     }
+    
 }
--- src/test/java/io/indrek/beer/BottlesTest.java
+++ src/test/java/io/indrek/beer/BottlesTest.java
@@ -3,7 +3,8 @@ package io.indrek.beer;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class BottlesTest {
 
@@ -19,6 +20,14 @@ public class BottlesTest {
         String expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
                 "Take one down and pass it around, 98 bottles of beer on the wall.";
 
-        assertEquals(expected, bottles.verse(99));
+        assertThat(bottles.verse(99), is(expected));
+    }
+
+    @Test
+    public void testAnotherVerse() throws Exception {
+        String expected = "89 bottles of beer on the wall, 89 bottles of beer.\n" +
+                "Take one down and pass it around, 88 bottles of beer on the wall.";
+
+        assertThat(bottles.verse(89), is(expected));
     }
 }
